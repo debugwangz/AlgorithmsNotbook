4e086d26cf55afc313c6e94b5b48e0eb7b8cdea2
wangzhe19950117 <1142179118@qq.com>
2021-01-27 15:24:25 +0800

    hello github

---
+++ AlgorithmsNotbook/AlgorithmsNotbook/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AlgorithmsNotbook
+//
+//  Created by 王哲 on 2021/1/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
